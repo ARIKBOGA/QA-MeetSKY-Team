cc5e674b12712a8a53ed53447ef356ebb6a8be9b
Asude-s <sati.turan1412@gmail.com>
2022-07-20 17:48:13 +0300

    new codes has been added

--- src/test/java/com/meetsky/pages/CalendarPage.java
+++ src/test/java/com/meetsky/pages/CalendarPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class CalendarPage {
+public class CalendarPage extends BasePage{
 
 
     @FindBy(xpath="//button[@class='button primary new-event']")
--- src/test/java/com/meetsky/step_definitions/Calendar_StepDefinitions.java
+++ src/test/java/com/meetsky/step_definitions/Calendar_StepDefinitions.java
@@ -8,10 +8,15 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Calendar;
 
 public class Calendar_StepDefinitions {
 
-    protected LoginPage loginPage = new LoginPage();
+    LoginPage loginPage = new LoginPage();
+    CalendarPage calendarPage = new CalendarPage();
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
 
     @When("User enters {string} and {string} and clicks on login button")
     public void userEntersAndAndClicksOnLoginButton(String username, String password) {
@@ -22,14 +27,18 @@ public class Calendar_StepDefinitions {
 
     @When("User clicks on calendar page link")
     public void user_clicks_on_calendar_page_link() {
+        calendarPage.calendarPageLink.click();
 
     }
     @When("User clicks on action item button")
     public void user_clicks_on_action_item_button() {
+        calendarPage.ActionItemButton.click();
+        calendarPage.DailyActionButton.click();
 
     }
     @Then("User should be display daily calendar view")
     public void user_should_be_display_daily_calendar_view() {
+
 
     }
 
